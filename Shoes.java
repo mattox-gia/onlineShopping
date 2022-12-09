c7e2c226f0842fb7d65bcb2e044c6354be547cb0
Gia Mattox <gmattox26@gmail.com>
2022-12-09 11:24:48 -0600

    h

--- Shoes.java
+++ Shoes.java
@@ -1,3 +1,42 @@
-public class Shoes extends Shirts{
-    
+public class Shoes extends Products{
+    private double shoeSize;
+    private String shoeColor;
+    private double shoePrice;
+
+    public Shoes(double shoeSize, String shoeColor, double shoePrice){
+        shoeSize = shoeSize;
+        shoeColor = shoeColor;
+        shoePrice = shoePrice;
+    }
+
+    public double getShoeSize() {
+        return shoeSize;
+    }
+
+    public void setShoeSize(double shoeSize) {
+        this.shoeSize = shoeSize;
+    }
+
+    public String getShoeColor() {
+        return shoeColor;
+    }
+
+    public void setShoeColor(String shoeColor) {
+        this.shoeColor = shoeColor;
+    }
+
+    public double getShoePrice() {
+        return shoePrice;
+    }
+
+    public void setShoePrice(double shoePrice) {
+        this.shoePrice = shoePrice;
+    }
+
+    public String toString(){
+        return "x";
+    }
+
 }
+
+
