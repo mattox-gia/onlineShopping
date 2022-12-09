9077517728ec66d72b83ce807beafe883630f199
Gia Mattox <gmattox26@gmail.com>
2022-12-09 08:51:19 -0600

    w

---
+++ Website.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Website{
+    private static ArrayList<Products> products = new ArrayList<>();
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        makeProducts();
+    }
+
+
+    public static void makeProducts(){
+        products.add(new)
+    }
+}
